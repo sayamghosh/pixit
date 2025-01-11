bad495f843c3712a7f3f5bb67e14a2c46891fb41
Sayam Ghosh <sayamghosh.dev@gmail.com>
2025-01-11 11:46:37 +0530

    loading updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,17 @@
 "use client";
-import React from 'react'
-import Card from './Card'
-import { useState,useEffect } from 'react';
-import Loadmore from './Loadmore';
+import React from "react";
+import Card from "./Card";
+import { useState, useEffect } from "react";
+import Loadmore from "./Loadmore";
 
-export default function Cards({type,query}) {
+export default function Cards({ type, query }) {
   const [curated, setCurated] = useState([]);
-  const [url,setUrl] = useState(type==="curated"?`https://api.pexels.com/v1/curated?per_page=24`:`https://api.pexels.com/v1/search?query=${query}&per_page=24`);
+  const [loading, setLoading] = useState(false);
+  const [url, setUrl] = useState(
+    type === "curated"
+      ? `https://api.pexels.com/v1/curated?per_page=24`
+      : `https://api.pexels.com/v1/search?query=${query}&per_page=24`
+  );
 
   async function searchCurated() {
     try {
@@ -19,20 +24,29 @@ export default function Cards({type,query}) {
       setCurated([...curated, ...data.photos]);
       setUrl(data.next_page);
       console.log("Response:", data);
+      setLoading(false);
     } catch (error) {
       console.error("Error:", error);
     }
   }
 
-  useEffect(()=>{
-    searchCurated()  //FIXME: It is called once or not 
-  },[])
+  useEffect(() => {
+    setLoading(true);
+    searchCurated(); //FIXME: It is called once or not
+  }, []);
   return (
     <>
-    <div className='mx-5 mt-4 sm:mx-10 lg:mx-20 grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 select-none '>
-        {curated.length > 0 && curated.map((item,index) => <Card key={index} data={item} />)}
-    </div>
-    <Loadmore searchCurated={searchCurated} />
+      <div className="mx-5 mt-4 sm:mx-10 lg:mx-20 grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 select-none ">
+        {curated.length > 0 &&
+          curated.map((item, index) => <Card key={index} data={item} />)}
+      </div>
+      {curated.length > 0 && <Loadmore searchCurated={searchCurated} />}
+      {loading && <p>Loading...</p>}
+      {!loading && curated.length === 0 && (
+        <h1 className="text-black max-w-screen-xl mx-auto font-bold font-serif text-2xl">
+          No photos found...
+        </h1>
+      )}
     </>
-  )
+  );
 }
